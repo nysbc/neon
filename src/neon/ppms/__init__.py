08ca5d763ee6f6b2ea97a9ad3b155673e71598fd
cesar mena <cmena@nysbc.org>
2025-09-11 12:11:17 -0700

    ...

---
+++ src/neon/ppms/__init__.py
@@ -0,0 +1,1 @@
+from .ppms import *
---
+++ src/neon/ppms/ppms.py
@@ -0,0 +1,60 @@
+from typing import Dict, List, Optional, cast
+
+import pyppms
+import pyppms.user
+
+
+class Group:
+    def __init__(self, data: Dict[str, str]) -> None:
+        self._data = data
+
+    @property
+    def name(self) -> str:
+        return self._data["unitlogin"]
+
+    @property
+    def pi_first(self) -> Optional[str]:
+        name = self._data["headname"].split(",")
+        if len(name) == 1:
+            return None
+        return name[1].strip()
+
+    @property
+    def pi_last(self) -> str:
+        name = self._data["headname"].split(",")
+        return name[0].strip()
+
+    @property
+    def pi_email(self) -> str:
+        return self._data["heademail"].strip().lower()
+
+    @property
+    def active(self) -> bool:
+        return bool(self._data["active"])
+
+    def __str__(self) -> str:
+        return f"[{self.name}|{self.pi_email}]"
+
+
+class Connection:
+    def __init__(self, key:str) -> None:
+        self._conn = pyppms.PpmsConnection(
+            "https://ppms.us/nysbc/pumapi/", key
+        )
+
+    def maybe_group(self, gid: str) -> Optional[Group]:
+        try:
+            return Group(self._conn.get_group(gid))
+        except KeyError:
+            return None
+
+    def group(self, gid: str) -> Group:
+        if (g := self.maybe_group(gid)) is None:
+            raise Exception()
+        return g
+
+    def groups(self) -> List[str]:
+        return cast(List[str], self._conn.get_groups())
+
+    def users_for_group(self, gid: str) -> List[pyppms.user.PpmsUser]:
+        return cast(List[pyppms.user.PpmsUser], self._conn.get_group_users(gid))
