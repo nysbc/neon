95a124790c695bc56714796f3fd2a97591b2a415
cesar mena <cmena@nysbc.org>
2025-09-16 10:34:40 -0700

    config: initial

--- src/neon/__init__.py
+++ src/neon/__init__.py
@@ -1,1 +1,11 @@
+import os
+from typing import Any, Dict, cast
+
 from . import ppms, redmine
+
+
+def config(section: str) -> Dict[str, Any]:
+    with open(os.environ["NEON_CONFIG"]) as config:
+        ns: Dict[str, Any] = {}
+        exec(config.read(), ns)
+        return cast(Dict[str, Any], ns[section])
