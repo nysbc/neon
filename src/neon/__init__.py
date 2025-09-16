8fe2aa4400cb5034bad1313ecc47862cf194f11a
cesar mena <cmena@nysbc.org>
2025-09-16 11:56:47 -0700

    neon/config: loosely type module ns

--- src/neon/__init__.py
+++ src/neon/__init__.py
@@ -6,6 +6,6 @@ from . import ppms, redmine
 
 def config(section: str) -> Dict[str, Any]:
     with open(os.environ["NEON_CONFIG"]) as config:
-        ns: Dict[str, Any] = {}
+        ns: Dict[Any, Any] = {}
         exec(config.read(), ns)
         return cast(Dict[str, Any], ns[section])
