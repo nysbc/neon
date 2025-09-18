0105056edfeed58e4e2ae4eb835a0a67171570cc
cesar mena <cmena@nysbc.org>
2025-09-17 20:25:51 -0700

    remove config()

--- src/neon/__init__.py
+++ src/neon/__init__.py
@@ -2,10 +2,3 @@ import os
 from typing import Any, Dict, cast
 
 from . import ppms, redmine
-
-
-def config(section: str) -> Dict[str, Any]:
-    with open(os.environ["NEON_CONFIG"]) as config:
-        ns: Dict[Any, Any] = {}
-        exec(config.read(), ns)
-        return cast(Dict[str, Any], ns[section])
